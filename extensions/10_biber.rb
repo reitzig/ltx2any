reitzig/ltx2any

commit 996f2b378ea833c8f6851aaa4fe4a36cc6283d69

Fixes the (re)copy routine executed at every start of a daemon-mode iteration.
 * Fixes issue #49: don't inadvertantly remove tmp files in pre-existing
	subfolders.
 * Fixes behaviour on symlinks in subfolders (unreported).
 * New method is adaptable to copy exceptions in subfolders.
	Not supported yet, but more possible now than before.
 * Adds message so the user sees where time is lost; copying used to
	be silent but it takes a while for larger projects.

ltx2any.rb
@@ -43,7 +43,7 @@ hashes    = ".hashes"
 params = ParameterManager.new # Out here for visibility at the end
 
 begin
-  puts "#{shortcode} Initialising ..." # TODO abstract away printing
+  print "#{shortcode} Initialising ..." # TODO abstract away printing
 
   # params = ParameterManager.new
 
@@ -297,7 +297,8 @@ begin
     $changetime = Time.now
     $jobfilelistener.start
   end
-
+  puts " Done"
+  
   begin # demon loop
     begin # inner block that can be cancelled by user
       # Reset
@@ -306,20 +307,41 @@ begin
       log.level = params[:loglevel]
       start_time = Time.now
 
+      print "#{shortcode} Copying files to tmp ..."
       # Copy all files to tmp directory (some LaTeX packages fail to work with output dir)
+      # excepting those we ignore anyways. Oh, and don't recurse outside the main
+      # directory, duh.
       exceptions = $ignoredfiles + $ignoredfiles.map { |s| "./#{s}" } + [".", ".."]
 
-      Dir.entries(".").delete_if { |f| exceptions.include?(f) }.each { |f|
-        # Avoid trouble with symlink loops
-        if ( File.symlink?(f) )
-          if ( !File.exists?("#{params[:tmpdir]}/#{f}") )
-            File.symlink("#{params[:jobpath]}/#{f}", "#{params[:tmpdir]}/#{f}")
+      define_singleton_method(:copy2tmp) { |files| 
+        files.each { |f|
+          if ( File.symlink?(f) )
+            # Avoid trouble with symlink loops
+            
+            # If a proper file or directory has been replaced with a symlink,
+            # remove the obsolete stuff
+            if (     File.exists?("#{params[:tmpdir]}/#{f}") \
+                 && !File.symlink?("#{params[:tmpdir]}/#{f}") )
+              FileUtils::rm_rf("#{params[:tmpdir]}/#{f}")
+            end
+            
+            # Create new symlink instead of copying 
+            if ( !File.exists?("#{params[:tmpdir]}/#{f}") )
+              File.symlink("#{params[:jobpath]}/#{f}", "#{params[:tmpdir]}/#{f}")
+            end
+          elsif ( File.directory?(f) )
+            copy2tmp(Dir.entries(f)\
+                        .delete_if { |s| [".", ".."]\
+                        .include?(s) }.map { |s| "#{f}/#{s}" })
+            # TODO Is this necessary? Why not just copy? (For now, safer and more adaptable.)
+          else
+            FileUtils::cp(f,"#{params[:tmpdir]}/")
           end
-        else
-          FileUtils::rm_rf("#{params[:tmpdir]}/#{f}")
-          FileUtils::cp_r(f,"#{params[:tmpdir]}/")
-        end
+        }
       }
+
+      copy2tmp(Dir.entries(".").delete_if { |f| exceptions.include?(f) })
+      puts " Done"
 
       # Move into temporary directory
       Dir.chdir(params[:tmpdir])

commit ad084766f0aa8773f9c0bad227c63bc90b7f6952

Introduces better progress indicator. Fixes.
 * Fixes bugs introduced with 996f2b378e; file copying is hopefully correct now.
 * Introduces nicer progress indication and integrates with affected extensions; implements issue #45.
 * Adds stub for DependencyManager

extensions/10_biber.rb
@@ -67,12 +67,11 @@ class Biber < Extension
     return usesbib && needrerun
   end
 
-  def exec()
+  def exec(progress)
     # Command to process bibtex bibliography if necessary.
     # Uses the following variables:
     # * jobname -- name of the main LaTeX file (without file ending)
     biber = '"biber \"#{@params[:jobname]}\""'
-    progress(3)
 
     f = IO::popen(eval(biber))
     log = f.readlines

extensions/10_bibtex.rb
@@ -80,12 +80,11 @@ class BibTeX < Extension
     return usesbib && needsrerun
   end
 
-  def exec()
+  def exec(progress)
     # Command to process bibtex bibliography if necessary.
     # Uses the following variables:
     # * jobname -- name of the main LaTeX file (without file ending)
     bibtex = '"bibtex \"#{@params[:jobname]}\""'
-    progress(3)
 
     f = IO::popen(eval(bibtex))
     log = f.readlines

extensions/20_makeindex.rb
@@ -33,7 +33,7 @@ class MakeIndex < Extension
        )
   end
 
-  def exec()
+  def exec(progress)
     # Command to create the index if necessary. Provide two versions,
     # one without and one with stylefile
     # Uses the following variables:
@@ -41,7 +41,6 @@ class MakeIndex < Extension
     # * mistyle -- name of the makeindex style file (with file ending)
     makeindex = { "default" => '"makeindex -q \"#{@params[:jobname]}\" 2>&1"',
                   "styled"  => '"makeindex -q -s \"#{mistyle}\" \"#{@params[:jobname]}\" 2>&1"'}
-    progress(3)
   
     version = "default"
     mistyle = nil

extensions/30_metapost.rb
@@ -27,13 +27,17 @@ class MetaPost < Extension
   end
 
   def do?
-    # Check whether there are any _.mp files that have changed
-    !Dir.entries(".").delete_if { |f|
-      (/\.mp$/ !~ f) || ($hashes.has_key?(f) && filehash(f) == $hashes[f])
-    }.empty?
+    job_size > 0
   end
 
-  def exec()
+  def job_size
+    # Count the number of changed _.mp files
+    Dir.entries(".").delete_if { |f|
+      (/\.mp$/ !~ f) || ($hashes.has_key?(f) && filehash(f) == $hashes[f])
+    }.size
+  end
+
+  def exec(progress)
     # Command to process metapost files if necessary.
     mpost = '"mpost -tex=#{@params[:engine]} -file-line-error -interaction=nonstopmode \"#{f}\" 2>&1"'
 
@@ -44,19 +48,13 @@ class MetaPost < Extension
 
     # Run mpost for each remaining file
     log = ""
-    c = 1
     begin # TODO move gem checking/loading to a central place?
       gem "parallel"
       require 'parallel'
       
       log = Parallel.map(mp_files) { |f|
         ilog = compile(mpost, f)
-        # Output up to ten dots
-        # TODO: make nicer output! Eg: [5/10]
-        if ( c % [1, (mp_files.size / 10)].max == 0 )
-          progress()
-        end
-        c += 1
+        progress.call
         ilog
       }.transpose
     rescue Gem::LoadError
@@ -66,12 +64,7 @@ class MetaPost < Extension
       
       mp_files.each { |f|
         log += compile(mpost, f)
-        # Output up to ten dots
-        # TODO: make nicer output! Eg: [5/10]
-        if ( c % [1, (mp_files.size / 10)].max == 0 )
-          progress()
-        end
-        c += 1
+        progress.call
       }
       log = log.transpose
     end

extensions/30_tikzext.rb
@@ -28,13 +28,22 @@ class TikZExt < Extension
   end
 
   def do?
-    File.exist?("#{@params[:jobname]}.figlist") && (@params[:imagerebuild] || 
-      IO.readlines("#{@params[:jobname]}.figlist").select { |fig|
-        !File.exist?("#{fig.strip}.pdf")
-      }.size > 0 )
+    job_size > 0
+  end
+  
+  def job_size
+    count = 0
+    
+    if ( File.exist?("#{@params[:jobname]}.figlist") )
+      count = IO.readlines("#{@params[:jobname]}.figlist").select { |fig|
+        @params[:imagerebuild] || !File.exist?("#{fig.strip}.pdf")
+      }.size
+    end
+      
+    return count
   end
 
-  def exec()
+  def exec(progress)
     # Command to process externalised TikZ images if necessary.
     # Uses the following variables:
     # * $params["engine"] -- Engine used by the main job.
@@ -53,19 +62,13 @@ class TikZExt < Extension
     
     # Run (latex) engine for each figure
     log = [[], ""]
-    c = 1
     begin # TODO move gem checking/loading to a central place?
       gem "parallel"
       require 'parallel'
       
       log = Parallel.map(figures) { |fig|
         ilog = compile(pdflatex, fig)
-        # Output up to ten dots
-        # TODO: make nicer output! Eg: [5/10]
-        if ( c % [1, (figures.size / 10)].max == 0 )
-          progress()
-        end
-        c += 1
+        progress.call
         ilog
       }.transpose
     rescue Gem::LoadError
@@ -75,12 +78,7 @@ class TikZExt < Extension
       
       figures.each { |fig|
         log += compile(pdflatex, fig)
-        # Output up to ten dots
-        # TODO: make nicer output! Eg: [5/10]
-        if ( c % [1, (figures.size / 10)].max == 0 )
-          progress()
-        end
-        c += 1
+        progress.call
       }
       log = log.transpose
     end

extensions/50_gnuplot.rb
@@ -27,13 +27,17 @@ class Gnuplot < Extension
   end
 
   def do?
-    # Check whether there are any _.gnuplot files that have changed
-    !Dir.entries(".").delete_if { |f|
-      (/\.gnuplot$/ !~ f) || ($hashes.has_key?(f) && filehash(f) == $hashes[f])
-    }.empty?
+    job_size > 0
   end
 
-  def exec()
+  def job_size
+    # Check whether there are any _.gnuplot files that have changed
+    Dir.entries(".").delete_if { |f|
+      (/\.gnuplot$/ !~ f) || ($hashes.has_key?(f) && filehash(f) == $hashes[f])
+    }.size
+  end
+
+  def exec(progress)
     # Command to process gnuplot files if necessary.
     # Uses the following variables:
     # * jobname -- name of the main LaTeX file (without file ending)
@@ -46,19 +50,13 @@ class Gnuplot < Extension
 
     # Run gnuplot for each remaining file
     log = ""
-    c = 1
     begin # TODO move gem checking/loading to a central place?
       gem "parallel"
       require 'parallel'
       
       log = Parallel.map(gnuplot_files) { |f|
         ilog = compile(gnuplot, f)
-        # Output up to ten dots
-        # TODO: make nicer output! Eg: [5/10]
-        if ( c % [1, (gnuplot_files.size / 10)].max == 0 )
-          progress()
-        end
-        c += 1
+        progress.call
         ilog
       }.transpose
     rescue Gem::LoadError
@@ -68,12 +66,7 @@ class Gnuplot < Extension
       
       gnuplot_files.each { |f|
         log += compile(gnuplot, f)
-        # Output up to ten dots
-        # TODO: make nicer output! Eg: [5/10]
-        if ( c % [1, (gnuplot_files.size / 10)].max == 0 )
-          progress()
-        end
-        c += 1
+        progress.call
       }
       log = log.transpose
     end

lib/DependencyManager.rb
@@ -1,4 +1,4 @@
-# Copyright 2010-2013, Raphael Reitzig
+# Copyright 2010-2015, Raphael Reitzig
 # <code@verrech.net>
 #
 # This file is part of ltx2any.
@@ -16,39 +16,23 @@
 # You should have received a copy of the GNU General Public License
 # along with ltx2any. If not, see <http://www.gnu.org/licenses/>.
 
-class Extension  
-  def self.dependencies
-    return []
-  end
-  
-  def initialize(params)
-    @name = "Dummy name"
-    @description = "Dummy description"
-    @parameters = []
-    @dependencies = []
-    @params = params
-  end
-  
-  public
-    def do?()
-      false
-    end
-    
-    def job_size
-      return 1
-    end
+require 'rubygems'
 
-    def exec(progress)
-      return [true, "No execution code, need to overwrite!"]
+class DependencyManager
+  def self.available?(type, name)
+    if ( type == :gem )
+      begin # TODO move gem checking/loading to a central place?
+        gem "#{name}"
+        require name
+        return true
+      rescue Gem::LoadError
+        Output.instance.msg("gem #{name} not available")
+      end
+      return false
+    elsif ( type == :binary )
+      Output.instance.msg("illegal dependency type")
+    else
+      Output.instance.msg("illegal dependency type")
     end
-
-    def to_s
-      @name
-    end
-    
-    attr_accessor :name, :description, :codes, :parameters
-    
-  protected
-    attr_reader :params, :dependencies
-    attr_writer :name, :description, :codes, :parameters
+  end
 end

lib/Output.rb
@@ -16,12 +16,23 @@
 # You should have received a copy of the GNU General Public License
 # along with ltx2any. If not, see <http://www.gnu.org/licenses/>.
 
-class Output
-  def initialize(name)
-    @shortcode = "[#{name}]"
+require 'singleton'
+
+class Output 
+  include Singleton
+  
+  def self.dependencies
+    return [["ruby-progressbar", :gem, :recommended, "for nice progress indicators"]]
+  end
+                  
+  def initialize() 
     @success   = "Done"
     @error     = "Error"
     @cancel    = "Cancelled"
+  end
+  
+  def name=(s)
+    @shortcode = "[#{s}]"
   end
 
   private
@@ -31,25 +42,40 @@ class Output
       }
     end
 
-  public
+  public    
     def msg(*msg)
       if ( msg.size > 0 )
         puts "#{@shortcode} #{msg[0]}"
-        if ( msg.length > 1 )
-          puts_indented(msg.drop(1))
-        end
+        puts_indented(msg.drop(1)) if msg.size > 1
       end
       STDOUT.flush
     end
     
-    def start(msg)
+    def start(msg, count=1)
+      # Set up progress bar if needed
+      if ( count > 1 && DependencyManager.available?(:gem, 'ruby-progressbar') )
+        progress = ProgressBar.create(:title => "#{@shortcode} #{msg} ...", 
+                                      :total => count,
+                                      :format => "%t [%c/%C]",
+                                      :autofinish => false)
+        return [lambda { progress.increment }, 
+                lambda { |state, *msgs| 
+                  progress.format("#{@shortcode} #{msg} ... #{instance_variable_get(("@#{state}").intern).to_s}" + (" " * 5)) # Add some spaces to hide all for up to 999 steps
+                  # TODO We *know* that we need 2*ceil(log_2(count)) - 1 spaces...
+                  progress.stop
+                  puts_indented(*msgs) if msgs.size > 0
+                  STDOUT.flush 
+                  }]
+      end  
+      # Fallback if progress bar not needed, or gem not available
       print "#{@shortcode} #{msg} ... "
       STDOUT.flush
+      return [lambda {}, lambda { |state, *msgs| stop(state, *msgs) }]
     end
     
     def stop(state, *msg)
       puts instance_variable_get(("@#{state}").intern).to_s
-      puts_indented(msg)
+      puts_indented(msg) if msg.size > 0
       STDOUT.flush
     end
     

ltx2any.rb
@@ -19,10 +19,9 @@
 
 require 'io/console'
 require 'fileutils'
-require 'rubygems'
 require 'digest'
 require 'yaml'
-Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |f| require f }
+Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |f| require f } # TODO load dependencies
 
 # Set process name to something less cumbersome
 $0="ltx2any.rb"
@@ -36,7 +35,8 @@ ignorefile = ".#{name}ignore_"
 hashes    = ".hashes"
 
 params = ParameterManager.new # Out here for visibility at the end
-output = Output.new(name)
+output = Output.instance
+output.name = name
 
 begin
   output.start("Initialising")
@@ -185,10 +185,10 @@ begin
     Digest::MD5.file(f).to_s
   end
 
-  def progress(steps=1) # TODO improve
-    print "." * steps
-    STDOUT.flush
-  end
+  #def progress(steps=1) # TODO improve
+  #  print "." * steps
+  #  STDOUT.flush
+  #end
 
   $ignoredfiles = ["#{ignorefile}#{params[:jobname]}",
                    "#{params[:tmpdir]}", 
@@ -313,24 +313,25 @@ begin
           if ( File.symlink?(f) )
             # Avoid trouble with symlink loops
             
+            # Delete old symlink if there is one, because:
             # If a proper file or directory has been replaced with a symlink,
-            # remove the obsolete stuff
-            if (     File.exists?("#{params[:tmpdir]}/#{f}") \
-                 && !File.symlink?("#{params[:tmpdir]}/#{f}") )
-              FileUtils::rm_rf("#{params[:tmpdir]}/#{f}")
+            # remove the obsolete stuff.
+            # If there already is a symlink, delete because it might have been
+            # relinked.
+            if ( File.exists?("#{params[:tmpdir]}/#{f}") )
+              FileUtils::rm("#{params[:tmpdir]}/#{f}")
             end
             
             # Create new symlink instead of copying 
-            if ( !File.exists?("#{params[:tmpdir]}/#{f}") )
-              File.symlink("#{params[:jobpath]}/#{f}", "#{params[:tmpdir]}/#{f}")
-            end
+            File.symlink("#{params[:jobpath]}/#{f}", "#{params[:tmpdir]}/#{f}")
           elsif ( File.directory?(f) )
+            FileUtils::mkdir_p("#{params[:tmpdir]}/#{f}")
             copy2tmp(Dir.entries(f)\
                         .delete_if { |s| [".", ".."]\
                         .include?(s) }.map { |s| "#{f}/#{s}" })
             # TODO Is this necessary? Why not just copy? (For now, safer and more adaptable.)
           else
-            FileUtils::cp(f,"#{params[:tmpdir]}/")
+            FileUtils::cp(f,"#{params[:tmpdir]}/#{f}")
           end
         }
       }
@@ -376,9 +377,9 @@ begin
         # Run all extensions in order
         extensions.each { |e| 
           if ( e.do? ) # TODO check dependencies
-            output.start("#{e.name} running")
-            r = e.exec()
-            output.stop(if r[0] then :success else :error end)
+            progress, stop = output.start("#{e.name} running", e.job_size)
+            r = e.exec(progress)
+            stop.call(if r[0] then :success else :error end)
             log.add_messages(e.name, :extension, r[1], r[2])
           end
         }